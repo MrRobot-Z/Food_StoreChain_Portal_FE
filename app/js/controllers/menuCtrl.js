0648cf408f3aa08fe3e99bba1e350bdcd9e91ba3
Moustafa Zaher <m.zaher2030@gmail.com>
2018-08-13 22:39:58 +0200

    Fixing Menu layout still in progress

--- app/js/controllers/menuCtrl.js
+++ app/js/controllers/menuCtrl.js
@@ -4,10 +4,27 @@
 
 'use strict';
 
-app.controller("MenuCtrl", function($scope){
+app.controller("MenuCtrl", function($scope, $http){
 
 
     $scope.ctrlName = "Hello this is menu controller";
+
+    $http({
+        method: 'GET',
+        url: '//localhost:8080/food-portal/menu'
+    }).then(function success(response) {
+        console.log("Yeeees");
+        console.log(response);
+        $scope.menu = response.data;
+    }, function error(response) {
+        console.log("Noooooooo");
+        console.log(response);
+    });
+
+
+    $scope.addToCart = function (item) {
+        console.log(item);
+    }
 
 });
 
